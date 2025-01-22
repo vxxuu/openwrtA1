129fe443bf2adf39c582bb0ad8db3cc6d0bbc976
vxxuu <vxxo88@gmail.com>
2025-01-22 21:32:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,17 +4,28 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2024 P3TERX
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 更新 feeds 并安装插件
+./scripts/feeds update -a
+./scripts/feeds install -a
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 安装额外的插件（确保所需插件都在 feeds 中）
+./scripts/feeds install luci-app-openclash
+./scripts/feeds install luci-app-passwall
+./scripts/feeds install luci-app-mosdns
+./scripts/feeds install luci-app-adguardhome
+./scripts/feeds install docker
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 修改默认 IP（可选）
+# sed -i 's/192.168.12.3/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题（可选）
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改主机名（可选）
+# sed -i 's/OpenWrt/Custom-Router/g' package/base-files/files/bin/config_generate
